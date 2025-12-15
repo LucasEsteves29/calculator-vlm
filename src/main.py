f83357c2aee76a1dd478d995af0450a62e41048a
Lucas Esteves <lucas.esteves@radixeng.com>
2025-12-15 18:04:02 -0300

    Main.py created

---
+++ src/main.py
@@ -0,0 +1,64 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_draw  = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+def lhand(conta2, conta3, conta4, conta5):
+    number = 0
+    if conta2>=0.15:
+        number+=1
+    if conta3>=0.20:
+        number+=1
+    if conta4>=0.18:
+        number+=1
+    if conta5>=0.09:
+        number+=1
+
+    return number
+
+with mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    model_complexity=1,
+    min_detection_confidence=0.6,
+    min_tracking_confidence=0.6
+) as hands:
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        frame = cv2.flip(frame, 1)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(rgb)
+
+        if results.multi_hand_landmarks:
+            for hand_lms in results.multi_hand_landmarks:
+
+                conta2 = hand_lms.landmark[5].y - hand_lms.landmark[8].y
+                conta3 = hand_lms.landmark[9].y - hand_lms.landmark[12].y
+                conta4 = hand_lms.landmark[13].y - hand_lms.landmark[16].y
+                conta5 = hand_lms.landmark[17].y - hand_lms.landmark[20].y
+
+                result = lhand(conta2, conta3, conta4, conta5)
+                cv2.putText(frame, str(result), (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                    cv2.LINE_AA)
+                mp_draw.draw_landmarks(
+                    frame, hand_lms, mp_hands.HAND_CONNECTIONS
+                )
+
+        cv2.imshow("Hands", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
